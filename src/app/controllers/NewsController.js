7d62322f2b21ea054ffff626a5cb34bdb34a0ae9
anhhuy <phamanhhuy22@gmail.com>
2023-04-04 22:40:50 +0700

    Upload Prettier Lint-staged husky

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,13 +1,13 @@
 class NewsController {
-	//[GET] /news
-	index(req, res) {
-		res.render("news");
-	}
+    //[GET] /news
+    index(req, res) {
+        res.render('news');
+    }
 
-	//[GET] /news/:slug --slug: anything path
-	show(req, res) {
-		res.send("NEWS DETAILS");
-	}
+    //[GET] /news/:slug --slug: anything path
+    show(req, res) {
+        res.send('NEWS DETAILS');
+    }
 }
 
 module.exports = new NewsController();
--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-const path = require("path");
+const path = require('path');
 
 //Express
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
 
 //Static files in Express
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true })); //Middleware for POST method
 app.use(express.json()); //Middleware for XMLHttpsrequest, axios, fetch, ajax...
 
 //Morgan
-const morgan = require("morgan");
+const morgan = require('morgan');
 // app.use(morgan("combined"));
 
 //Handlebars
-const handlebars = require("express-handlebars");
-const exp = require("constants");
+const handlebars = require('express-handlebars');
+const exp = require('constants');
 
-app.engine(".hbs", handlebars.engine({ extname: ".hbs" })); //Change .handlebars -> .hbs
-app.set("view engine", ".hbs");
-app.set("views", path.join(__dirname, "/resource/views"));
+app.engine('.hbs', handlebars.engine({ extname: '.hbs' })); //Change .handlebars -> .hbs
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, '/resource/views'));
 
 //Route init
-const route = require("./routes");
+const route = require('./routes');
 route(app);
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
