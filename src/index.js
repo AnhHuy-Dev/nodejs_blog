4d5115d20964ef7438e493d5693ea2e6a57b60be
anhhuy <phamanhhuy22@gmail.com>
2023-04-04 20:59:31 +0700

    Upload Routes&Controller

--- src/index.js
+++ src/index.js
@@ -7,21 +7,24 @@ const port = 3000;
 
 //Static files in Express
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true })); //Middleware for POST method
+app.use(express.json()); //Middleware for XMLHttpsrequest, axios, fetch, ajax...
 
 //Morgan
 const morgan = require("morgan");
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 //Handlebars
 const handlebars = require("express-handlebars");
+const exp = require("constants");
 
 app.engine(".hbs", handlebars.engine({ extname: ".hbs" })); //Change .handlebars -> .hbs
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "/resource/views"));
 
-app.get("/", (req, res) => {
-	res.render("home");
-});
+//Route init
+const route = require("./routes");
+route(app);
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
