7eadf2218ac5abb3bbda43b3c2bd400f2b0b9104
anhhuy <phamanhhuy22@gmail.com>
2023-04-13 21:47:23 +0700

    Add course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -10,6 +10,18 @@ class CourseController {
             .catch(next);
         // res.send("COURSES NEWS");
     }
+
+    //GET /courses/create
+    create(req, res, next) {
+        res.render('courses/create');
+    }
+
+    //POST /courses/store
+    store(req, res, next) {
+        const formData = req.body;
+        formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+        Course.create(formData).then(() => res.redirect('/'));
+    }
 }
 
 module.exports = new CourseController();
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
 
-const Course = new Schema({
-    name: String, // String is shorthand for {type: String}
-    title: String,
-    image: String,
-    slug: String,
-    videoId: String,
-    createdAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
-});
+const Course = new Schema(
+    {
+        name: String, // String is shorthand for {type: String}
+        title: String,
+        image: String,
+        slug: String,
+        videoId: String,
+        slug: { type: String, slug: 'name', unique: true },
+    },
+    {
+        timestamps: true,
+    },
+);
 
 const courses = mongoose.model('Course', Course);
 
